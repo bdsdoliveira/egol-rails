b180fa75853098b775891deda2d4e6398d31bbb6
Bruno Oliveira <bdsdoliveira@gmail.com>
2013-04-15 19:56:44 -0300

    initial rails and android apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EhGol::Application.config.secret_token = 'dfdcf69a61935309f346eb61b2579b9ba9ecdd9073dd2a6727e9d7193db3fba1e484ec209accdcfffe924097b60f0596f4628c08c7f170435365c461fc91a0ab'
