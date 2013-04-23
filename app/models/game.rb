68da93bf835184071833c4bddc838e7831557fd0
Bruno Oliveira <bdsdoliveira@gmail.com>
2013-04-23 12:27:08 -0300

    relationships fully working

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,10 @@
 class Game < ActiveRecord::Base
-  attr_accessible :date_and_time, :score_team1, :score_team2
+  attr_accessible :date_and_time, :score_team1, :score_team2, :stage, :stage_id, :city, :city_id, :team1, :team1_id, :team2, :team2_id
 
   belongs_to :city
   belongs_to :stage
-  belongs_to :team1, :class_name => 'Team', :foreign_key => 'team_id'
-  belongs_to :team2, :class_name => 'Team', :foreign_key => 'team_id'
+  belongs_to :team1, :class_name => 'Team', :foreign_key => 'team1_id'
+  belongs_to :team2, :class_name => 'Team', :foreign_key => 'team2_id'
 
 #  def teams
 #    Team.find(:conditions => ["team1_id = ? OR team2_id = ?", id, id])
