80ad0e74a370230ce1103628a7e4a9fce9a48d58
Bruno Oliveira <bdsdoliveira@gmail.com>
2013-06-02 21:38:40 -0300

    add static_pages; home layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 EhGol::Application.routes.draw do
-  resources :stages
+  get "static_pages/home"
 
+  get "static_pages/help"
+
+  get "static_pages/about"
+
+  resources :stages
 
   resources :teams
 
-
   resources :cities
-
 
   resources :games
 
   # Mobile access to list of games
   match '/m' => 'games#m'
 
+  root :to => 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -63,7 +67,7 @@ EhGol::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'games#index'
+  # root :to => 'public#index'
 
   # See how all your routes lay out with "rake routes"
 
