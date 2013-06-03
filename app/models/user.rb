1114eedd615ba7cf6221a8163426b1621dc2c7a3
Bruno Oliveira <bdsdoliveira@gmail.com>
2013-06-02 22:39:30 -0300

    add index to user email and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
+
+  before_save { |user| user.email = email.downcase }
 end
