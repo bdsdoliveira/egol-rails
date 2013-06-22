c9a506e3c0dbad8f617b15190793a2c3f1e924ab
Bruno Oliveira <bdsdoliveira@gmail.com>
2013-06-21 22:36:46 -0300

    home and match layout greatly improved

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,18 @@
 class StaticPagesController < ApplicationController
+
+  before_filter :label_colors, only: [:home, :match]
+
+  def label_colors
+    @LABEL_COLORS = {
+      1 => "label-warning",
+      2 => "label-success",
+      3 => "label-important",
+      4 => "label-info",
+      5 => "",
+      6 => "label-inverse"
+    }
+  end
+
   def home
     @matches = Match.all
   end
